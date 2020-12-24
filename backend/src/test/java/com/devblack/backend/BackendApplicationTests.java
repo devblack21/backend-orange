06ae37759d4c8acc940e0e9c4ca1621c2e398a61
Marcos <nyrocha018@gmail.com>
2020-12-24 13:57:01 -0300

    subindo estrutura inicial do projeto

---
+++ backend/src/test/java/com/devblack/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devblack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
